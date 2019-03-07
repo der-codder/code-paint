cb726cf5eb84e1e0d2ac96a83ac380e40711b373
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2019-03-07 16:06:35 +0200

    Initialize WebApi project with tests

---
+++ CodePaint.WebApi.Tests/ValuesControllerTest.cs
@@ -0,0 +1,23 @@
+using System;
+using CodePaint.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace CodePaint.WebApi.Tests
+{
+    public class ValuesControllerTest
+    {
+        [Fact]
+        public void ValuesController_Get_Should_Return_Result()
+        {
+            //Arrange
+            var controller = new ValuesController();
+
+            //Act
+            var result = controller.Get(0);
+
+            //Assert
+            Assert.IsType<ActionResult<string>>(result);
+        }
+    }
+}

99513b1f2c9f08d3738d12362db0f702498c8734
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2019-03-08 10:00:45 +0200

    Rename CodePaint.WebApi folder to server,CodePaint.WebApi.Tests to tests

--- server/Controllers/ValuesController.cs
+++ server/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace CodePaint.WebApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value3", "value2" };
         }
 
         // GET api/values/5

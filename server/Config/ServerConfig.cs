3d63963a19dbf5273ce9bf66881936924e68488c
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2019-03-09 11:21:28 +0200

    Add mongoDB support for server

---
+++ server/Config/ServerConfig.cs
@@ -0,0 +1,7 @@
+namespace CodePaint.WebApi
+{
+    public class ServerConfig
+    {
+        public MongoDBConfig MongoDB { get; set; } = new MongoDBConfig();
+    }
+}
--- server/Startup.cs
+++ server/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,6 +25,9 @@ namespace CodePaint.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var config = new ServerConfig();
+            Configuration.Bind(config);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 

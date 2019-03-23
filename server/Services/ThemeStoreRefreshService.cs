2c92c2c26078411cbfcf51e61b5512c30fb997f4
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2019-03-23 13:07:29 +0200

    Add ThemeStoreRefreshService

---
+++ server/Services/ThemeStoreRefreshService.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodePaint.WebApi.Domain.Models;
+using Polly;
+using Polly.Retry;
+using Serilog;
+
+namespace CodePaint.WebApi.Services
+{
+    public interface IThemeStoreRefreshService
+    {
+        Task RefreshGalleryStore(List<GalleryItem> metadata);
+        Task RefreshGalleryStoreTheme(GalleryItem freshThemeMetadata);
+    }
+
+    public class ThemeStoreRefreshService : IThemeStoreRefreshService
+    {
+        private readonly IThemeStoreRefresher _refresher;
+
+        private readonly AsyncRetryPolicy _policy = Policy
+            .Handle<Exception>()
+            .WaitAndRetryAsync(
+                5,
+                _ => TimeSpan.FromSeconds(5),
+                (_, ts) => Log.Error($"Error while processing fresh theme. Retrying in {ts.Seconds} sec.")
+            );
+
+        public ThemeStoreRefreshService(IThemeStoreRefresher refresher) => _refresher = refresher;
+
+        public async Task RefreshGalleryStore(List<GalleryItem> metadata)
+        {
+            Log.Information("Gallery Store Refreshing Started.");
+            const int numberOfItemsPerIteration = 10;
+            for (var i = 0; numberOfItemsPerIteration * i <= metadata.Count; i++)
+            {
+                var itemsToRefresh = metadata
+                    .Skip(numberOfItemsPerIteration * i)
+                    .Take(numberOfItemsPerIteration);
+
+                await Task.WhenAll(
+                    itemsToRefresh
+                        .Select(item => RefreshGalleryStoreTheme(item))
+                        .ToArray()
+                );
+            }
+            Log.Information("Gallery Store Refreshing Completed.");
+        }
+
+        public async Task RefreshGalleryStoreTheme(GalleryItem freshThemeMetadata)
+        {
+            var savedGalleryItemType = await _refresher.GetSavedGalleryItemType(freshThemeMetadata.Id);
+            if (savedGalleryItemType != GalleryItemType.Default)
+            {
+                Log.Information($"Skip refreshing of theme: Id='{freshThemeMetadata.Id}'; Type='{savedGalleryItemType}'.");
+                return;
+            }
+
+            Log.Information($"Refresh gallery store theme: Id='{freshThemeMetadata.Id}'.");
+            var storedTheme = await _refresher.GetStoredTheme(freshThemeMetadata.Id);
+
+            if (storedTheme != null && storedTheme.Version == freshThemeMetadata.Version)
+            {
+                Log.Information($"Theme '{storedTheme.Id}' does not changed.");
+                return;
+            }
+
+            var freshTheme = await _policy.ExecuteAsync(
+                async () => await _refresher.DownloadFreshTheme(freshThemeMetadata)
+            );
+
+            var freshThemeType = await _refresher.CheckAndUpdateFreshThemeType(freshTheme);
+            if (freshThemeType == GalleryItemType.Default)
+            {
+                if (storedTheme == null)
+                {
+                    await _refresher.CreateTheme(freshTheme);
+                }
+                else
+                {
+                    await _refresher.UpdateTheme(freshTheme);
+                }
+            }
+        }
+    }
+}
---
+++ tests/Services/ThemeStoreRefreshServiceTests.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using CodePaint.WebApi.Domain.Models;
+using CodePaint.WebApi.Services;
+using Moq;
+using Xunit;
+
+namespace CodePaint.WebApi.Tests.Services
+{
+    public class ThemeStoreRefreshServiceTests : IDisposable
+    {
+        private readonly AutoMock _mock;
+
+        public ThemeStoreRefreshServiceTests()
+        {
+            _mock = AutoMock.GetLoose();
+        }
+
+        public void Dispose()
+        {
+            _mock.Dispose();
+        }
+
+        [Fact]
+        public void RefreshGalleryStore_ProcessesAllGivenItems()
+        {
+            var items = new List<GalleryItem>();
+            const int itemsCount = 12;
+            for (int i = 0; i < itemsCount; i++)
+            {
+                items.Add(new GalleryItem { Id = $"test_{i}" });
+            }
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetSavedGalleryItemType(It.IsAny<string>()))
+                .ReturnsAsync(GalleryItemType.NoThemes);
+            var mockRefreshService = _mock.Create<ThemeStoreRefreshService>();
+
+            mockRefreshService.RefreshGalleryStore(items).Wait();
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(
+                    x => x.GetSavedGalleryItemType(It.IsAny<string>()),
+                    Times.Exactly(itemsCount)
+                );
+        }
+
+        [Theory]
+        [InlineData(GalleryItemType.NoThemes)]
+        [InlineData(GalleryItemType.NeedAttention)]
+        public void RefreshGalleryStoreTheme_TakesThemeWithNonDefaultType_DoesNotDownloadsFreshTheme(
+            GalleryItemType extensionType)
+        {
+            var galleryItem = new GalleryItem { Id = "expectedId_test" };
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetSavedGalleryItemType(galleryItem.Id))
+                .ReturnsAsync(extensionType);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.DownloadFreshTheme(galleryItem))
+                .ReturnsAsync(new VSCodeTheme());
+            var mockRefreshService = _mock.Create<ThemeStoreRefreshService>();
+
+            mockRefreshService.RefreshGalleryStoreTheme(galleryItem).Wait();
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.GetSavedGalleryItemType(galleryItem.Id), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.DownloadFreshTheme(galleryItem), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(GalleryItemType.NoThemes)]
+        [InlineData(GalleryItemType.NeedAttention)]
+        public void RefreshGalleryStoreTheme_TakesNonDefaultTypeTheme_DoesNotRefreshTheme(
+            GalleryItemType extensionType)
+        {
+            var galleryItem = new GalleryItem { Id = "expectedId_test" };
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetSavedGalleryItemType(galleryItem.Id))
+                .ReturnsAsync(extensionType);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.CreateTheme(It.IsAny<VSCodeTheme>()));
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.UpdateTheme(It.IsAny<VSCodeTheme>()));
+            var mockRefreshService = _mock.Create<ThemeStoreRefreshService>();
+
+            mockRefreshService.RefreshGalleryStoreTheme(galleryItem).Wait();
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.GetSavedGalleryItemType(galleryItem.Id), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.CreateTheme(It.IsAny<VSCodeTheme>()), Times.Never);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.CreateTheme(It.IsAny<VSCodeTheme>()), Times.Never);
+        }
+
+        [Fact]
+        public void RefreshGalleryStoreTheme_TakesThemeWithActualVersion_DoesNotDownloadsFreshTheme()
+        {
+            var galleryItem = new GalleryItem { Id = "expectedId_test", Version = "version_test" };
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetSavedGalleryItemType(galleryItem.Id))
+                .ReturnsAsync(GalleryItemType.Default);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetStoredTheme(galleryItem.Id))
+                .ReturnsAsync(new VSCodeTheme { Version = galleryItem.Version });
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.DownloadFreshTheme(galleryItem))
+                .ReturnsAsync(new VSCodeTheme());
+            var mockRefreshService = _mock.Create<ThemeStoreRefreshService>();
+
+            mockRefreshService.RefreshGalleryStoreTheme(galleryItem).Wait();
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.GetSavedGalleryItemType(galleryItem.Id), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.GetStoredTheme(galleryItem.Id), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.DownloadFreshTheme(galleryItem), Times.Never);
+        }
+
+        [Fact]
+        public void RefreshGalleryStoreTheme_TakesThemeWithActualVersion_DoesNotRefreshTheme()
+        {
+            var galleryItem = new GalleryItem { Id = "expectedId_test", Version = "version_test" };
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetSavedGalleryItemType(galleryItem.Id))
+                .ReturnsAsync(GalleryItemType.Default);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetStoredTheme(galleryItem.Id))
+                .ReturnsAsync(new VSCodeTheme { Version = galleryItem.Version });
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.CreateTheme(It.IsAny<VSCodeTheme>()));
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.UpdateTheme(It.IsAny<VSCodeTheme>()));
+            var mockRefreshService = _mock.Create<ThemeStoreRefreshService>();
+
+            mockRefreshService.RefreshGalleryStoreTheme(galleryItem).Wait();
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.GetSavedGalleryItemType(galleryItem.Id), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.GetStoredTheme(galleryItem.Id), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.CreateTheme(It.IsAny<VSCodeTheme>()), Times.Never);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.UpdateTheme(It.IsAny<VSCodeTheme>()), Times.Never);
+        }
+
+        [Fact]
+        public void RefreshGalleryStoreTheme_TakesNewTheme_DownloadsAndCreateIt()
+        {
+            const string expectedId = "expectedId_test";
+            const string expectedVersion = "version1";
+            var newGalleryItem = new GalleryItem { Id = expectedId, Version = expectedVersion };
+            var colorTheme = new Theme
+            {
+                TokenColors = new List<TokenColor> { new TokenColor { Name = "name_test" } }
+            };
+            var newTheme = new VSCodeTheme
+            {
+                Id = expectedId,
+                Version = expectedVersion,
+                Themes = new List<Theme> { colorTheme }
+            };
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetSavedGalleryItemType(newGalleryItem.Id))
+                .ReturnsAsync(GalleryItemType.Default);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetStoredTheme(newGalleryItem.Id))
+                .ReturnsAsync(() => null);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.DownloadFreshTheme(newGalleryItem))
+                .ReturnsAsync(newTheme);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.CreateTheme(newTheme))
+                .Returns(Task.CompletedTask);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.UpdateTheme(It.IsAny<VSCodeTheme>()));
+            var mockRefreshService = _mock.Create<ThemeStoreRefreshService>();
+
+            mockRefreshService.RefreshGalleryStoreTheme(newGalleryItem).Wait();
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.GetSavedGalleryItemType(newGalleryItem.Id), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.DownloadFreshTheme(newGalleryItem), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.CreateTheme(newTheme), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.UpdateTheme(It.IsAny<VSCodeTheme>()), Times.Never);
+        }
+
+        [Fact]
+        public void RefreshGalleryStoreTheme_TakesNewNoThemesExtension_UpdatesExtensionTypeMetadata_And_DoesNotRefreshExtension()
+        {
+            const string expectedId = "expectedId_test";
+            const string expectedVersion = "version1";
+            var newGalleryItem = new GalleryItem { Id = expectedId, Version = expectedVersion };
+            var newTheme = new VSCodeTheme
+            {
+                Id = expectedId,
+                Version = expectedVersion,
+                Themes = new List<Theme>()
+            };
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetSavedGalleryItemType(newGalleryItem.Id))
+                .ReturnsAsync(GalleryItemType.Default);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetStoredTheme(newGalleryItem.Id))
+                .ReturnsAsync(() => null);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.DownloadFreshTheme(newGalleryItem))
+                .ReturnsAsync(newTheme);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.CheckAndUpdateFreshThemeType(newTheme))
+                .ReturnsAsync(GalleryItemType.NoThemes);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.CreateTheme(It.IsAny<VSCodeTheme>()));
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.UpdateTheme(It.IsAny<VSCodeTheme>()));
+            var mockRefreshService = _mock.Create<ThemeStoreRefreshService>();
+
+            mockRefreshService.RefreshGalleryStoreTheme(newGalleryItem).Wait();
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.GetSavedGalleryItemType(newGalleryItem.Id), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.DownloadFreshTheme(newGalleryItem), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.CheckAndUpdateFreshThemeType(newTheme), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.CreateTheme(It.IsAny<VSCodeTheme>()), Times.Never);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.UpdateTheme(It.IsAny<VSCodeTheme>()), Times.Never);
+        }
+
+        [Fact]
+        public void RefreshGalleryStoreTheme_TakesNewNeedAttentionExtension_UpdatesExtensionTypeMetadata_And_DoesNotRefreshExtension()
+        {
+            const string expectedId = "expectedId_test";
+            const string expectedVersion = "version1";
+            var newGalleryItem = new GalleryItem { Id = expectedId, Version = expectedVersion };
+            var newTheme = new VSCodeTheme
+            {
+                Id = expectedId,
+                Version = expectedVersion,
+                Themes = new List<Theme> { new Theme() }
+            };
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetSavedGalleryItemType(newGalleryItem.Id))
+                .ReturnsAsync(GalleryItemType.Default);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetStoredTheme(newGalleryItem.Id))
+                .ReturnsAsync(() => null);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.DownloadFreshTheme(newGalleryItem))
+                .ReturnsAsync(newTheme);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.CheckAndUpdateFreshThemeType(newTheme))
+                .ReturnsAsync(GalleryItemType.NeedAttention);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.CreateTheme(It.IsAny<VSCodeTheme>()));
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.UpdateTheme(It.IsAny<VSCodeTheme>()));
+            var mockRefreshService = _mock.Create<ThemeStoreRefreshService>();
+
+            mockRefreshService.RefreshGalleryStoreTheme(newGalleryItem).Wait();
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.GetSavedGalleryItemType(newGalleryItem.Id), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.DownloadFreshTheme(newGalleryItem), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.CheckAndUpdateFreshThemeType(newTheme), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.CreateTheme(It.IsAny<VSCodeTheme>()), Times.Never);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.UpdateTheme(It.IsAny<VSCodeTheme>()), Times.Never);
+        }
+
+        [Fact]
+        public void RefreshGalleryStoreTheme_TakesFreshTheme_DownloadsAndUpdateIt()
+        {
+            const string expectedId = "expectedId_test";
+            var colorTheme = new Theme
+            {
+                TokenColors = new List<TokenColor> { new TokenColor { Name = "name_test" } }
+            };
+            var freshGalleryItem = new GalleryItem { Id = expectedId, Version = "version2" };
+            var oldTheme = new VSCodeTheme
+            {
+                Id = expectedId,
+                Version = "version1",
+                Themes = new List<Theme> { colorTheme }
+            };
+            var freshTheme = new VSCodeTheme
+            {
+                Id = expectedId,
+                Version = "version2",
+                Themes = new List<Theme> { colorTheme }
+            };
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetSavedGalleryItemType(freshGalleryItem.Id))
+                .ReturnsAsync(GalleryItemType.Default);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.GetStoredTheme(freshGalleryItem.Id))
+                .ReturnsAsync(oldTheme);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.DownloadFreshTheme(freshGalleryItem))
+                .ReturnsAsync(freshTheme);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.CreateTheme(It.IsAny<VSCodeTheme>()));
+            _mock.Mock<IThemeStoreRefresher>()
+                .Setup(x => x.UpdateTheme(freshTheme))
+                .Returns(Task.CompletedTask);
+            var mockRefreshService = _mock.Create<ThemeStoreRefreshService>();
+
+            mockRefreshService.RefreshGalleryStoreTheme(freshGalleryItem).Wait();
+
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.GetSavedGalleryItemType(freshGalleryItem.Id), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.DownloadFreshTheme(freshGalleryItem), Times.Once);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.CreateTheme(It.IsAny<VSCodeTheme>()), Times.Never);
+            _mock.Mock<IThemeStoreRefresher>()
+                .Verify(x => x.UpdateTheme(freshTheme), Times.Once);
+        }
+    }
+}

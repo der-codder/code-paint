c6f0b7532d3399c3bb90c57a6119a4550d50581c
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2019-03-23 13:06:50 +0200

    Add ThemeStoreRefresher

---
+++ server/Services/ThemeStoreRefresher.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodePaint.WebApi.Domain.Models;
+using CodePaint.WebApi.Domain.Repositories;
+using Polly;
+using Polly.Retry;
+using Serilog;
+
+namespace CodePaint.WebApi.Services
+{
+    public interface IThemeStoreRefresher
+    {
+        Task<GalleryItemType> GetSavedGalleryItemType(string extensionId);
+        Task<VSCodeTheme> GetStoredTheme(string extensionId);
+        Task<VSCodeTheme> DownloadFreshTheme(GalleryItem metadata);
+        Task<GalleryItemType> CheckAndUpdateFreshThemeType(VSCodeTheme freshTheme);
+        Task CreateTheme(VSCodeTheme newTheme);
+        Task UpdateTheme(VSCodeTheme theme);
+    }
+
+    public class ThemeStoreRefresher : IThemeStoreRefresher
+    {
+        private readonly IVSMarketplaceClient _marketplaceClient;
+        private readonly IVSExtensionHandler _extensionHandler;
+        private readonly IVSCodeThemeStoreRepository _storeRepository;
+        private readonly IGalleryItemsRepository _galleryItemsRepository;
+
+        public ThemeStoreRefresher(
+            IVSMarketplaceClient marketplaceClient,
+            IVSExtensionHandler extensionHandler,
+            IVSCodeThemeStoreRepository storeRepository,
+            IGalleryItemsRepository galleryInfoRepository)
+        {
+            _marketplaceClient = marketplaceClient;
+            _extensionHandler = extensionHandler;
+            _storeRepository = storeRepository;
+            _galleryItemsRepository = galleryInfoRepository;
+        }
+
+        public async Task<GalleryItemType> GetSavedGalleryItemType(string extensionId)
+        {
+            var savedThemeMetadata = await _galleryItemsRepository.GetGalleryItem(extensionId);
+
+            return savedThemeMetadata.Type;
+        }
+
+        public async Task<VSCodeTheme> GetStoredTheme(string extensionId)
+        {
+            return await _storeRepository.GetTheme(extensionId);
+        }
+
+        public async Task<VSCodeTheme> DownloadFreshTheme(GalleryItem metadata)
+        {
+            VSCodeTheme freshTheme;
+            using (var stream = await _marketplaceClient.GetVsixFileStream(metadata))
+            {
+                freshTheme = await _extensionHandler.ProcessExtension(stream);
+            }
+
+            return freshTheme;
+        }
+
+        public async Task<GalleryItemType> CheckAndUpdateFreshThemeType(VSCodeTheme freshTheme)
+        {
+            if (freshTheme.Themes.Count == 0)
+            {
+                Log.Information($"Extension '{freshTheme.Id}' does not contribute any theme. Changing its Type.");
+                const GalleryItemType themeType = GalleryItemType.NoThemes;
+                var result = await _galleryItemsRepository.ChangeGalleryItemType(
+                    freshTheme.Id,
+                    themeType
+                );
+
+                if (result)
+                {
+                    Log.Information($"Successfully changed Type to {themeType}.");
+                }
+                else
+                {
+                    Log.Warning($"Type change unsuccessful: ExtensionId: '{freshTheme.Id}'.");
+                }
+
+                return themeType;
+            }
+            if (freshTheme.Themes.Any(t => t.TokenColors.Count == 0))
+            {
+                Log.Information($"Extension '{freshTheme.Id}' does not contains any TokenColor. Changing its Type.");
+                const GalleryItemType themeType = GalleryItemType.NeedAttention;
+                var result = await _galleryItemsRepository.ChangeGalleryItemType(
+                    freshTheme.Id,
+                    themeType
+                );
+
+                if (result)
+                {
+                    Log.Information($"Successfully changed Type to {themeType}.");
+                }
+                else
+                {
+                    Log.Warning($"Type change unsuccessful: ExtensionId: '{freshTheme.Id}'.");
+                }
+
+                return themeType;
+            }
+
+            return GalleryItemType.Default;
+        }
+
+        public async Task CreateTheme(VSCodeTheme newTheme)
+        {
+            await _storeRepository.Create(newTheme);
+            Log.Information($"Created new theme in store: Id='{newTheme.Id}'.");
+        }
+
+        public async Task UpdateTheme(VSCodeTheme theme)
+        {
+            var result = await _storeRepository.Update(theme);
+
+            if (result)
+            {
+                Log.Information($"Successfuly updated theme in store: ThemeId='{theme.Id}'.");
+            }
+            else
+            {
+                Log.Warning($"Update unsuccessful theme in store: ThemeId='{theme.Id}'.");
+            }
+        }
+    }
+}
---
+++ tests/Services/ThemeStoreRefresherTests.cs
@@ -0,0 +1,188 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using CodePaint.WebApi.Domain.Models;
+using CodePaint.WebApi.Domain.Repositories;
+using CodePaint.WebApi.Services;
+using Moq;
+using Xunit;
+
+namespace CodePaint.WebApi.Tests.Services
+{
+    public class ThemeStoreRefresherTests : IDisposable
+    {
+        private readonly AutoMock _mock;
+
+        public ThemeStoreRefresherTests()
+        {
+            _mock = AutoMock.GetLoose();
+        }
+
+        public void Dispose()
+        {
+            _mock.Dispose();
+        }
+
+        [Fact]
+        public void GetSavedGalleryItemType_ReturnsProperType()
+        {
+            const string expectedId = "expectedId_test";
+            const GalleryItemType expectedGalleryItemType = GalleryItemType.NoThemes;
+            _mock.Mock<IGalleryItemsRepository>()
+                .Setup(x => x.GetGalleryItem(expectedId))
+                .ReturnsAsync(new GalleryItem { Type = expectedGalleryItemType });
+            var mockRefresher = _mock.Create<ThemeStoreRefresher>();
+
+            var actual = mockRefresher.GetSavedGalleryItemType(expectedId).Result;
+
+            Assert.True(expectedGalleryItemType == actual);
+        }
+
+        [Fact]
+        public void GetStoredTheme_ReturnsProperTheme()
+        {
+            const string expectedId = "expectedId_test";
+            _mock.Mock<IVSCodeThemeStoreRepository>()
+                .Setup(x => x.GetTheme(expectedId))
+                .ReturnsAsync(new VSCodeTheme { Id = expectedId });
+            var mockRefresher = _mock.Create<ThemeStoreRefresher>();
+
+            var actual = mockRefresher.GetStoredTheme(expectedId).Result;
+
+            Assert.Equal(expectedId, actual.Id);
+        }
+
+        [Fact]
+        public void CheckAndUpdateFreshThemeType_FreshThemeContainsTokenColors_ReturnsDefaultType()
+        {
+            var freshTheme = new VSCodeTheme
+            {
+                Themes = new List<Theme>
+                {
+                    new Theme
+                    {
+                        TokenColors = new List<TokenColor>
+                        {
+                            new TokenColor { Name = "name_test" }
+                        }
+                    }
+                }
+            };
+            var mockRefresher = _mock.Create<ThemeStoreRefresher>();
+
+            var actualType = mockRefresher.CheckAndUpdateFreshThemeType(freshTheme).Result;
+
+            Assert.Equal(GalleryItemType.Default, actualType);
+        }
+
+        [Fact]
+        public void CheckAndUpdateFreshThemeType_FreshThemeDoesNotContainsAnyTokenColors_ReturnsNeedAttentionType_And_UpdatesMetadata()
+        {
+            const string expectedId = "expectedId_test";
+            const GalleryItemType expectedType = GalleryItemType.NeedAttention;
+            var freshTheme = new VSCodeTheme
+            {
+                Id = expectedId,
+                Themes = new List<Theme>
+                {
+                    new Theme()
+                }
+            };
+
+            _mock.Mock<IGalleryItemsRepository>()
+                .Setup(x => x.ChangeGalleryItemType(expectedId, expectedType))
+                .ReturnsAsync(true);
+            var mockRefresher = _mock.Create<ThemeStoreRefresher>();
+
+            var actualType = mockRefresher.CheckAndUpdateFreshThemeType(freshTheme).Result;
+
+            Assert.Equal(expectedType, actualType);
+        }
+
+        [Fact]
+        public void CheckAndUpdateFreshThemeType_FreshThemeDoesNotContributesAnyThemes_ReturnsNoThemesType_And_UpdatesMetadata()
+        {
+            const string expectedId = "expectedId_test";
+            const GalleryItemType expectedType = GalleryItemType.NoThemes;
+            var freshTheme = new VSCodeTheme
+            {
+                Id = expectedId,
+                Themes = new List<Theme>()
+            };
+
+            _mock.Mock<IGalleryItemsRepository>()
+                .Setup(x => x.ChangeGalleryItemType(expectedId, expectedType))
+                .ReturnsAsync(true);
+            var mockRefresher = _mock.Create<ThemeStoreRefresher>();
+
+            var actualType = mockRefresher.CheckAndUpdateFreshThemeType(freshTheme).Result;
+
+            Assert.Equal(expectedType, actualType);
+        }
+
+        [Fact]
+        public void CreateTheme_CreatesNewThemeInStore()
+        {
+            const string expectedId = "expectedId_test";
+            var newTheme = new VSCodeTheme { Id = expectedId };
+            _mock.Mock<IVSCodeThemeStoreRepository>()
+                .Setup(x => x.Create(newTheme))
+                .Returns(Task.CompletedTask);
+            var mockRefresher = _mock.Create<ThemeStoreRefresher>();
+
+            mockRefresher.CreateTheme(newTheme).Wait();
+
+            _mock.Mock<IVSCodeThemeStoreRepository>()
+                .Verify(x => x.Create(newTheme), Times.Once);
+        }
+
+        [Fact]
+        public void UpdateTheme_UpdatesThemeInStore()
+        {
+            const string expectedId = "expectedId_test";
+            var theme = new VSCodeTheme { Id = expectedId };
+            _mock.Mock<IVSCodeThemeStoreRepository>()
+                .Setup(x => x.Update(theme))
+                .ReturnsAsync(true);
+            var mockRefresher = _mock.Create<ThemeStoreRefresher>();
+
+            mockRefresher.UpdateTheme(theme).Wait();
+
+            _mock.Mock<IVSCodeThemeStoreRepository>()
+                .Verify(x => x.Update(theme), Times.Once);
+        }
+
+        [Fact]
+        public void DownloadFreshTheme_ReturnsProperTheme()
+        {
+            var metadata = new GalleryItem
+            {
+                PublisherName = "espectedPublisherName_test",
+                Name = "espectedName_test",
+                Version = "espectedVersion_test"
+            };
+            var expectedTheme = new VSCodeTheme
+            {
+                Id = $"{metadata.PublisherName}.{metadata.Name}",
+                Version = metadata.Version
+            };
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(""));
+
+            _mock.Mock<IVSMarketplaceClient>()
+                .Setup(x => x.GetVsixFileStream(metadata))
+                .ReturnsAsync(stream);
+            _mock.Mock<IVSExtensionHandler>()
+                .Setup(x => x.ProcessExtension(stream))
+                .ReturnsAsync(expectedTheme);
+            var mockRefresher = _mock.Create<ThemeStoreRefresher>();
+
+            var actualTheme = mockRefresher.DownloadFreshTheme(metadata).Result;
+
+            Assert.Equal(expectedTheme.Id, actualTheme.Id);
+            Assert.Equal(expectedTheme.Version, actualTheme.Version);
+        }
+    }
+}
